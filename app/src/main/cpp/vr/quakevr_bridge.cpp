d8ffdeee693c1745c9c33d2bebf2ecb7942b1312
realzelda414 <102430399+realzelda414@users.noreply.github.com>
2026-08-19 19:21:39 -0400

    Update quakevr_bridge.cpp

--- app/src/main/cpp/vr/quakevr_bridge.cpp
+++ app/src/main/cpp/vr/quakevr_bridge.cpp
@@ -1,10 +1,11 @@
 #include "quakevr_bridge.hpp"
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
 #include <android/log.h>
-#include <android_native_app_glue.h>
 #include <cmath>
 #include <cstring>
 
-#define LOG_TAG "QuakeVR-OpenXR"
+#define LOG_TAG "QuakeVR-Bridge"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
 
