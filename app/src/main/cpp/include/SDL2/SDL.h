57bb79bbc0dd4845331ab3a533430d453b35e82f
realzelda414 <102430399+realzelda414@users.noreply.github.com>
2026-08-19 20:43:35 -0400

    Update SDL.h

--- app/src/main/cpp/include/SDL2/SDL.h
+++ app/src/main/cpp/include/SDL2/SDL.h
@@ -2,6 +2,10 @@
 #include <stdint.h>
 #include <stddef.h>
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef uint8_t Uint8;
 typedef uint16_t Uint16;
 typedef uint32_t Uint32;
@@ -13,3 +17,53 @@ typedef int64_t Sint64;
 typedef int SDL_bool;
 #define SDL_TRUE 1
 #define SDL_FALSE 0
+
+typedef struct SDL_Window SDL_Window;
+typedef void* SDL_GLContext;
+
+typedef struct SDL_DisplayMode {
+    Uint32 format;
+    int w;
+    int h;
+    int refresh_rate;
+    void *driverdata;
+} SDL_DisplayMode;
+
+#define SDL_BUTTON_LEFT 1
+#define SDL_BUTTON_MIDDLE 2
+#define SDL_BUTTON_RIGHT 3
+#define SDL_BUTTON_X1 4
+#define SDL_BUTTON_X2 5
+#define SDL_BUTTON(X) (1 << ((X)-1))
+#define SDL_BUTTON_LMASK SDL_BUTTON(SDL_BUTTON_LEFT)
+#define SDL_BUTTON_MMASK SDL_BUTTON(SDL_BUTTON_MIDDLE)
+#define SDL_BUTTON_RMASK SDL_BUTTON(SDL_BUTTON_RIGHT)
+
+inline Uint32 SDL_GetMouseState(int *x, int *y) {
+    if (x) *x = 0;
+    if (y) *y = 0;
+    return 0;
+}
+
+inline Uint32 SDL_GetRelativeMouseState(int *x, int *y) {
+    if (x) *x = 0;
+    if (y) *y = 0;
+    return 0;
+}
+
+inline void SDL_WarpMouseInWindow(SDL_Window *window, int x, int y) {
+    (void)window; (void)x; (void)y;
+}
+
+inline int SDL_SetRelativeMouseMode(SDL_bool enabled) {
+    (void)enabled;
+    return 0;
+}
+
+inline SDL_bool SDL_GetRelativeMouseMode(void) {
+    return (SDL_bool)0;
+}
+
+#ifdef __cplusplus
+}
+#endif
