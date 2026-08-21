e120c55a3d849951a8443285b2ee9e80a3d5b02f
realzelda414 <102430399+realzelda414@users.noreply.github.com>
2026-08-21 17:11:54 -0400

    Update android_main.cpp

--- app/src/main/cpp/android_main.cpp
+++ app/src/main/cpp/android_main.cpp
@@ -1,4 +1,5 @@
 #include "common.hpp"
+#include "host.hpp"
 #include <android_native_app_glue.h>
 #include <android/log.h>
 #include <unistd.h>
@@ -8,12 +9,6 @@
 #define LOG_TAG "QuakeVR-Main"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-
-// Quake Host function signatures
-void Host_Init(quakeparms_t *parms);
-void _Host_Frame(double time);
-void Host_Shutdown(void);
-double Sys_DoubleTime(void);
 
 // OpenXR Bridge Declarations
 namespace quake {
